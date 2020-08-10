819c0be187dd432e3ec82c245fb3eea8509db123
Marton <hunmarton@hotmail.com>
2020-08-10 18:55:21 +0200

    Added js for scroll to top and sticky navbar done

---
+++ app.js
@@ -0,0 +1,24 @@
+//to the top scroll
+const upArrow = document.getElementById('arrowUp');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+        upArrow.classList.add('visible');
+    } else {
+        upArrow.classList.remove('visible');
+    }
+    console.log(window.scrollY);
+});
+upArrow.addEventListener('click', () => {
+    //document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+});
+const navBar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) {
+        navBar.style.top = 0;
+    }
+    else {
+        navBar.style.top = 90 + 'px';
+    }
+});
